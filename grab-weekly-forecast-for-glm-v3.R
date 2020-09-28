aaf24aff3da965a963a6fcb172694dcd2d30a88d
QuinnThomas <rqthomas@vt.edu>
2020-09-28 12:49:22 -0400

    Update grab-weekly-forecast-for-glm-v3.R
    Updates to new noaa GEFS forecast

--- grab-weekly-forecast-for-glm-v3.R
+++ grab-weekly-forecast-for-glm-v3.R
@@ -15,6 +15,7 @@ lake_lon_w_list = c(79.9, 82.017318, 72.058199, 89.4739973, 149.61051, 99.25302)
 lake_name_list = c("fcre", "sugg", "sunp", "cram", "tool", "prpo")
 
 directory = '/data/NOAA/'
+
 if(!file.exists(directory)) {
   dir.create(file.path(directory))
 }
@@ -28,8 +29,8 @@ for(subdir in lake_name_list) {
 #####
 
 time <- c(0, 64) #6 hour prediction for 16 days
-lon.dom <- seq(0, 360, by = 1) #domain of longitudes in model
-lat.dom <- seq(-90, 90, by = 1) #domain of latitudes in model
+lon.dom <- seq(0, 359, by = 0.5) #domain of longitudes in model (1 degree resolution)
+lat.dom <- seq(-90, 90, by = 0.5) #domain of latitudes in model (1 degree resolution)
 
 for(lake_index in 1:length(lake_name_list)){
   
@@ -41,10 +42,14 @@ for(lake_index in 1:length(lake_name_list)){
   
   for(i in 1:length(urls.out$url)){
     
+    urls.out$model <- "gefs"
+    urls.out$date <- urls.out$date[which(lubridate::as_date(urls.out$date) > lubridate::as_date("2020-09-25"))]
+    urls.out$url <- paste0("https://nomads.ncep.noaa.gov:443/dods/gefs/gefs",urls.out$date)
+    
     model.url <- urls.out$url[i]
-    model.url <- str_replace(model.url,"gens_bc","gens")
+    #model.url <- str_replace(model.url,"gens_bc","gens")
     run_date <- urls.out$date[i]
-    model_list <- c('gep_all_00z', 'gep_all_06z', 'gep_all_12z', 'gep_all_18z')
+    model_list <- c("gefs_pgrb2ap5_all_00z", "gefs_pgrb2ap5_all_06z", "gefs_pgrb2ap5_all_12z", "gefs_pgrb2ap5_all_18z")
     
     for(m in 1:length(model_list)){
       
@@ -82,34 +87,36 @@ for(lake_index in 1:length(lake_name_list)){
           #tcdcclm #entire atmosphere total cloud cover [%]
           
           tmp2m <- DODSGrab(model.url, model.run, "tmp2m", time = time, lon = c(lon,lon), 
-                            lat = c(lat,lat),ensembles=c(0,20))
+                            lat = c(lat,lat),ensembles=c(0,30))
           
           dlwrfsfc <- DODSGrab(model.url, model.run, "dlwrfsfc", time = time, lon = c(lon,lon), 
-                               lat = c(lat,lat),ensembles=c(0,20))
+                               lat = c(lat,lat),ensembles=c(0,30))
           
           dswrfsfc <- DODSGrab(model.url, model.run, "dswrfsfc", time = time, lon = c(lon,lon), 
-                               lat = c(lat,lat),ensembles=c(0,20))
+                               lat = c(lat,lat),ensembles=c(0,30))
           
-          pratesfc <- DODSGrab(model.url, model.run, "pratesfc", time = time, lon = c(lon,lon), 
-                               lat = c(lat,lat),ensembles=c(0,20))
+          apcpsfc <- DODSGrab(model.url, model.run, "apcpsfc", time = time, lon = c(lon,lon), 
+                               lat = c(lat,lat),ensembles=c(0,30))
+          
+          apcpsfc$value <- apcpsfc$value / (60 * 60 * 6)
           
           rh2m <- DODSGrab(model.url, model.run, "rh2m", time = time, lon = c(lon,lon), 
-                           lat = c(lat,lat),ensembles=c(0,20))
+                           lat = c(lat,lat),ensembles=c(0,30))
           
           vgrd10m <- DODSGrab(model.url, model.run, "vgrd10m", time = time, lon = c(lon,lon), 
-                              lat = c(lat,lat),ensembles=c(0,20))
+                              lat = c(lat,lat),ensembles=c(0,30))
           
           ugrd10m <- DODSGrab(model.url, model.run, "ugrd10m", time = time, lon = c(lon,lon), 
-                              lat = c(lat,lat),ensembles=c(0,20))
+                              lat = c(lat,lat),ensembles=c(0,30))
           
-          spfh2m <- DODSGrab(model.url, model.run, "spfh2m", time = time, lon = c(lon,lon), 
-                             lat = c(lat,lat),ensembles=c(0,20))
+          #spfh2m <- DODSGrab(model.url, model.run, "spfh2m", time = time, lon = c(lon,lon), 
+          #                   lat = c(lat,lat),ensembles=c(0,30))
           
           pressfc <- DODSGrab(model.url, model.run, "pressfc", time = time, lon = c(lon,lon), 
-                              lat = c(lat,lat),ensembles=c(0,20))
+                              lat = c(lat,lat),ensembles=c(0,30))
           
           tcdcclm <- DODSGrab(model.url, model.run, "tcdcclm", time = time, lon = c(lon,lon), 
-                              lat = c(lat,lat),ensembles=c(0,20))
+                              lat = c(lat,lat),ensembles=c(0,30))
           
           forecast.time <- strftime(tmp2m$forecast.date, format="%Y-%m-%d %H:%M:%S",tz = 'GMT')
           
@@ -118,11 +125,11 @@ for(lake_index in 1:length(lake_name_list)){
                                       tmp2m = tmp2m$value, 
                                       dlwrfsfc= dlwrfsfc$value, 
                                       dswrfsfc = dswrfsfc$value, 
-                                      pratesfc = pratesfc$value, 
+                                      pratesfc = apcpsfc$value, 
                                       rh2m = rh2m$value, 
                                       vgrd10m = vgrd10m$value, 
                                       ugrd10m = ugrd10m$value, 
-                                      spfh2m = spfh2m$value, 
+                                      #spfh2m = spfh2m$value, 
                                       pressfc = pressfc$value,
                                       tcdcclm = tcdcclm$value)
           
@@ -132,4 +139,3 @@ for(lake_index in 1:length(lake_name_list)){
     }
   }
 }
-
